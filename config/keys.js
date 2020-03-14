babfaed8525b35e6a98f031e7fb4d11b5860cce7
Jamiebullock87 <jamiebullock1987@gmail.com>
2020-03-14 22:25:47 +0000

    clean up

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://theRealDenzel:Qd55YNUZCJt7jj2d@pied-piper-gjjmn.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI: `${process.env.CONNSTR}`,
     secretOrKey: "secret",
 };
